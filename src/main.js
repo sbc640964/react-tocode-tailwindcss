7ec046c44e8cbfbaa46042676edf3152678cb024
simcha <sbc640964@gmail.com>
2021-02-07 23:04:12 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import '../css/main.css';
+
+const App = () => {
+
+  return (
+    <>123</>
+  )
+};
+
+
+// main.js
+const root = document.querySelector('#unlimitedwp_app');
+ReactDOM.render(<App />, root);
